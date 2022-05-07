f38f8378597f2b3a1ec24a80bde16fe11d04c422
Filipe Alves De Souza <filipewallace19@gmail.com>
2022-05-06 20:27:18 -0400

    filled out filmDao, added override methods to daoImpl

--- src/main/java/com/skilldistillery/film/dao/FilmDAO.java
+++ src/main/java/com/skilldistillery/film/dao/FilmDAO.java
@@ -1,5 +1,19 @@
 package com.skilldistillery.film.dao;
 
-public interface FilmDAO {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.skilldistillery.film.entities.Actor;
+import com.skilldistillery.film.entities.Category;
+import com.skilldistillery.film.entities.Film;
+
+public interface FilmDAO {
+	  public Film findFilmById(int filmId);
+	  public Actor findActorById(int actorId);
+	  public List<Actor> findActorsByFilmId(int filmId);
+	  public ArrayList<Integer> searchFilm(String searchTerm);
+	  public List<Category> findCategoriesByFilmId(int filmId);
+	  public Film createFilm(Film film);
+	  public boolean deleteFilm(Film film);
+	  public Film updateFilm(Film film);
 }
--- src/main/java/com/skilldistillery/film/dao/FilmDAOJdbcImpl.java
+++ src/main/java/com/skilldistillery/film/dao/FilmDAOJdbcImpl.java
@@ -1,8 +1,63 @@
 package com.skilldistillery.film.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Component;
+
+import com.skilldistillery.film.entities.Actor;
+import com.skilldistillery.film.entities.Category;
+import com.skilldistillery.film.entities.Film;
 
 @Component
 public class FilmDAOJdbcImpl implements FilmDAO {
+
+	@Override
+	public Film findFilmById(int filmId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Actor findActorById(int actorId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Actor> findActorsByFilmId(int filmId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Integer> searchFilm(String searchTerm) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Category> findCategoriesByFilmId(int filmId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Film createFilm(Film film) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deleteFilm(Film film) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Film updateFilm(Film film) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
